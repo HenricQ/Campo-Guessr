4dd74d1543329b9622ee17e26e5e0cf339906f7a
HenricQ <Henricooq@gmail.com>
2024-12-10 15:20:29 -0400

    agora sim é a final antes da apresentação

--- assets/scriptGuessr.js
+++ assets/scriptGuessr.js
@@ -100,7 +100,7 @@ function initMap() {
           map: map,
           title: 'Novo Marcador',
           icon:{
-            url: 'assets/images/redPill.png',
+            url: 'images/redPill.png',
             scaledSize: new google.maps.Size(48, 48)
         } 
         });
@@ -183,7 +183,7 @@ function initMap() {
             map: map,
             title: 'Street View',
             icon:{
-                url: 'assets/images/bluePill.png',
+                url: 'images/bluePill.png',
                 scaledSize: new google.maps.Size(48, 48)
             }    
           });
